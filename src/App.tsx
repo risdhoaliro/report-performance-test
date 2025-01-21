71a418de413db72f5d1ca658404512e3b77333ac
risdhoaliro <risdhoaliro@gmail.com>
2025-01-21 21:29:29 +0700

    delete import react

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, LineChart, Line, ResponsiveContainer, AreaChart, Area } from 'recharts';
 
